8919408dd434d06e43df2afdd8800ef510cc2163
programmerhasan001 <programmermdhasan@gmail.com>
2024-07-30 16:42:53 +0600

    admin dashboard

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar from '../ui/dashboard/sidebar/Sidebar'
+import Navbar from '../ui/dashboard/navbar/Navbar'
+import styles from "../ui/dashboard/dashboard.module.css"
+
+export default function Layout({ children }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.menu}>
+                <Sidebar />
+            </div>
+            <div className={styles.content}>
+                <Navbar />
+                {children}
+            </div>
+        </div>
+    )
+}
---
+++ app/ui/dashboard/sidebar/menuLink/MenuLink.jsx
@@ -0,0 +1,14 @@
+"use client"
+import Link from "next/link"
+import styles from "./menuLink.module.css"
+import { usePathname } from "next/navigation"
+
+export default function MenuLink({ item }) {
+    const pathName = usePathname()
+    return (
+        <Link href={item.path} className={`${styles.container} ${pathName === item.path && styles.active}`}>
+            {item.icon}
+            {item.title}
+        </Link>
+    )
+}
